42d90faf7f5a19570b0496058cfae1cefdf177a5
Mateus Sipauba <mateus.sipauba@yahoo.com.br>
2025-10-23 19:13:46 -0300

    Funcionando de forma rudimentar, mas funcionando.

--- function_schedule.py
+++ function_schedule.py
@@ -2,14 +2,25 @@ import schedule
 import time
 import threading
 from function_check_difference import function_check_difference
+from function_copy import function_copy
+from frame_entry_clone import entry_clone
+from frame_entry_original import entry_original
 
 def function_schedule(intervalo_horas=1):
     """
     Agenda a execução automática da cópia a cada X horas.
     Mantém o Tkinter livre (roda em thread separada).
     """
+
+    from frame_entry_clone import entry_clone
+    from frame_entry_original import entry_original
+
+    path_original = entry_original.get()
+    path_clone = entry_clone.get()
+
     schedule.clear()
-    schedule.every(intervalo_horas).minutes.do(function_check_difference)
+    schedule.every(intervalo_horas).minutes.do(
+        lambda:function_copy(path_original,path_clone))
 
     def run_schedule():
         while True:
